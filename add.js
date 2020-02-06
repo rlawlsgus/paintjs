842a5c13e12d8b84bb757f86f2606cf452670ba4
rlawlsgus <jh0623.kim@gmail.com>
2020-02-06 16:52:08 +0900

    Initial Commit

---
+++ add.js
@@ -0,0 +1,37 @@
+const canvas = document.getElementById("jsCanvas");
+const ctx = canvas.getContext("2d");
+
+ctx.strokeStyle = "#2C2C2C";
+ctx.lineWidth = 2.5;
+
+canvas.width = 700;
+canvas.height = 700;
+
+let painting = false;
+
+function stopPainting() {
+    painting = false;
+}
+
+function startPainting() {
+    painting = true;
+}
+
+function onMouseMove(event) {
+    const x = event.offsetX;
+    const y = event.offsetY;
+    if(!painting) {
+        ctx.beginPath();
+        ctx.moveTo(x, y);
+    } else {
+        ctx.lineTo(x, y);
+        ctx.stroke();
+    }
+}
+
+if(canvas) {
+    canvas.addEventListener("mousemove", onMouseMove);
+    canvas.addEventListener("mousedown", startPainting);
+    canvas.addEventListener("mouseup", stopPainting);
+    canvas.addEventListener("mouseleave", stopPainting);
+}
